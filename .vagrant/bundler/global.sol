aea60d1d7abc04e0999afd3839f6bdec28fb07c5
Vagrant User <vagrant@controle>
2024-11-23 19:11:38 +0200

    Primeiro commit

---
+++ .vagrant/bundler/global.sol
@@ -0,0 +1,1 @@
+{"dependencies":[["vagrant-disksize",["= 0.1.3"]],["vagrant-group",["= 0.8.2"]]],"checksum":"8cd5966ab97c7cddc747f43681577db1a0d9eb7599919ad76174074d101f04cd","vagrant_version":"2.4.1"}
